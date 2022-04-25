4fd730f34ee4f9fcd1103ab14d07a8c5e8ba33b4
saintkim12 <saintkim1232@gmail.com>
2022-04-25 12:16:30 +0900

    spa test

--- prototype-react/vite.config.ts
+++ prototype-react/vite.config.ts
@@ -13,5 +13,11 @@ export default defineConfig((env: ConfigEnv) => ({
   base: env.command === 'build' ? '/plan-i-prototype/' : '/',
   build: {
     outDir: '../docs/',
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'index.html'),
+        '404': resolve(__dirname, '404.html'),
+      }
+    }
   }
 }))
