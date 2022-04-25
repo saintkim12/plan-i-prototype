14394e393d05216cb4dd7c9ecf2c88f4c30f7a03
saintkim12 <saintkim1232@gmail.com>
2022-04-25 12:02:36 +0900

    build test

--- prototype-react/vite.config.ts
+++ prototype-react/vite.config.ts
@@ -1,16 +1,17 @@
 import { resolve } from 'path'
-import { defineConfig } from 'vite'
+import { ConfigEnv, defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig((env: ConfigEnv) => ({
   plugins: [react()],
   resolve: {
     alias: {
       '/src/': `${resolve(__dirname, './src')}/`,
     },
   },
+  base: env.command === 'build' ? '/plan-i-prototype/' : '/',
   build: {
-    outDir: 'build'
+    outDir: '../docs/',
   }
-})
+}))
