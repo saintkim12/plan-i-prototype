ca7e48d61805cf351d03cc70f33ac3fdc7353e1e
saintkim12 <saintkim1232@gmail.com>
2022-04-28 21:41:17 +0900

    css test

---
+++ prototype-react/src/components/Wrapper.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  height: 100vh;
+  background-color: #282c34;
+  text-align: center;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+
+export default Wrapper
