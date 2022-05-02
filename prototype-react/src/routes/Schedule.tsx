8437f54da5a7efa2560205853ef1caad12f3b1f0
saintkim12 <saintkim1232@gmail.com>
2022-05-02 18:56:12 +0900

    화면 레이아웃 작업중

--- prototype-react/src/routes/Schedule.tsx
+++ prototype-react/src/routes/Schedule.tsx
@@ -1,195 +1,47 @@
-import localForage from 'localforage'
-import flow from 'lodash/fp/flow'
-import { DateTime } from 'luxon'
-import { useCallback, useEffect, useMemo, useState } from 'react'
-import Timetable from '/src/components/Timetable'
+import { Component } from 'react'
+import styled from 'styled-components'
+// import Wrapper from '/src/components/Wrapper'
 
-type GoogleCalendarEventTime = { dateTime: string, timeZone: string }
-type GoogleCalendarEventItem = { id: string, summary: string, start: GoogleCalendarEventTime, end: GoogleCalendarEventTime, [key: string]: any }
-type EventItem = { username: string, id: string, title: string, start: string, end: string, }
-interface GoogleOAuth2Token {
-  accessToken: string
-  tokenType: string
-  expiresIn: number
-  scope: string[]
-}
-interface CustomToken {
-  createdIn: number
-}
-type GoogleToken = GoogleOAuth2Token & CustomToken
-
-const CLIENT_ID = import.meta.env.VITE_GOOGLE_API_CLIENT_ID
-const REDIRECT_URI = import.meta.env.VITE_GOOGLE_API_REDIRECT_URI
-const API_KEY = import.meta.env.VITE_GOOGLE_API_API_KEY
-function loginToGoogle() {
-  location.href = `https://accounts.google.com/o/oauth2/v2/auth?${new URLSearchParams({
-    'client_id': CLIENT_ID,
-    'redirect_uri': REDIRECT_URI,
-    'response_type': 'token',
-    'include_granted_scopes': 'true',
-    'scope': [
-      'https://www.googleapis.com/auth/calendar',
-      'https://www.googleapis.com/auth/calendar.events',
-      'https://www.googleapis.com/auth/calendar.events.readonly',
-      'https://www.googleapis.com/auth/calendar.readonly',
-    ].join(' ')
-  })}`
-}
-
-function getEvents<T extends GoogleOAuth2Token>(token: T | null, option?: { username?: string, calendarId?: string, timeZone?: 'Asia/Seoul' | 'Asia/Tokyo', timeMin?: string, timeMax?: string }) {
-  if (!token) return Promise.resolve([])
-  const calendarId = option?.calendarId
-  if (!calendarId) return Promise.resolve([])
-  const username = option?.username
-  const timeZone = option?.timeZone ?? 'Asia/Seoul'
-  const timeMin = flow(s => s ? `${s}:00Z` : '')(option?.timeMin as string)
-  const timeMax = flow(s => s ? `${s}:00Z` : '')(option?.timeMax as string)
-  console.log('timeMin', timeMin, 'timeMax', timeMax)
-  
-  return fetch(`https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events?${new URLSearchParams({
-    key: API_KEY,
-    timeZone: timeZone,
-    timeMin: timeMin,
-    timeMax: timeMax,
-  }).toString()}`, {
-    method: 'get',
-    headers: {
-      'Authorization': `${token.tokenType} ${token.accessToken}`,
-      'Content-Type': 'application/json;charset=UTF-8',
-    }
-  }).then(r => r.json()).then(data => {
-    /*
-     * accessRole: "owner"
-     * defaultRemindars: [{…}]
-     * etag: "\"p32sfdp6vtaofe0g\""
-     * items: (19) [{…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}]
-     *    created: "2022-04-26T03:32:20.000Z"
-     *    creator: {email: 'kiparseo@gmail.com', self: true}
-     *    end: {dateTime: '2022-04-26T10:45:00+09:00', timeZone: 'Asia/Seoul'}
-     *    etag: "\"3301888799612000\""
-     *    eventType: "default"
-     *    htmlLink: "https://www.google.com/calendar/event?eid=MDczMG9kMjc2dGVnNmFmcnFyM2NnMXN2YTIga2lwYXJzZW9AbQ"
-     *    iCalUID: "0730od276teg6afrqr3cg1sva2@google.com"
-     *    id: "*"
-     *    kind: "calendar#event"
-     *    organizer: {email: 'kiparseo@gmail.com', self: true}
-     *    remindars: {useDefault: true}
-     *    sequence: 2
-     *    start: {dateTime: '2022-04-26T09:45:00+09:00', timeZone: 'Asia/Seoul'}
-     *    status: "confirmed"
-     *    summary: "대충만든일정"
-     *    updated: "2022-04-26T03:39:59.806Z"
-     * kind: "calendar#events"
-     * nextSyncToken: "*"
-     * summary: "kiparseo@gmail.com"
-     * timeZone: "Asia/Seoul"
-     * updated: "2022-04-26T03:56:04.147Z"
-     */
-    // console.log('data', data)
-    const items: GoogleCalendarEventItem[] = data?.items ?? []
-    return items.map((o: GoogleCalendarEventItem) => ({ username: username, id: o.id, title: o.summary, start: o.start.dateTime, end: o.end.dateTime } as EventItem))
-  })
-}
-
-export default function Schedule({ $interviewerList = [] }) {
-  const [interviewerList, setInterviewerList] = useState<{ username: string, calendarList: { type: string, id: string }[] }[]>([])
-
-  /** test */
-  const [$testInterviewInput, $setTestInterviewInput] = useState('tester/saintkim1232@gmail.com,tester2/kiparseo@gmail.com')
-  useEffect(() => {
-    setInterviewerList(
-      Array.from($testInterviewInput.split(',')).map(s => s.trim()).filter(s => !!s).map(s => s.split('/')).map(([username, id]) => ({ username, calendarList: [{ type: 'google', id }] }))
-    )
-  }, [$testInterviewInput])
-  /** test */
-
-  const [token, setToken] = useState<GoogleToken | null>(null)
-  const [timeMin, setTimeMin] = useState('2022-01-01T00:00')
-  const [timeMax, setTimeMax] = useState('2022-12-31T23:59')
-  const [eventList, setEventList] = useState<any[]>([])
-  const accessToken = useMemo(() => token?.accessToken, [token])
-  const isTokenValid = useMemo(() => {
-    return token?.accessToken && token?.createdIn && token?.expiresIn
-      && (DateTime.fromMillis(token.createdIn).plus({ milliseconds: token.expiresIn }).diffNow().toMillis() > 0)
-  }, [token])
-
-  const loadEventList = useCallback(() => {
-    console.log('loadEventList')
-    const googleCalendarIdList: { username: string, calendarId: string }[] = interviewerList.flatMap(({ username, calendarList }) => calendarList.filter(({ type }) => type === 'google').map(({ id }) => ({ username, calendarId: id })))
-    return Promise.allSettled(
-      googleCalendarIdList.map(({ username, calendarId }) => getEvents(token, { username, calendarId, timeMin, timeMax }))
-    ).then((results) => {
-      return results.filter(r => r.status === 'fulfilled').map(r => (r as PromiseFulfilledResult<typeof eventList>).value)
-        .flatMap(m => m)
-    }).then(setEventList)
-    // getEvents(token, { timeMin, timeMax }).then(setEventList)
-  }, [token])
-
-  useEffect(() => {
-    /*
-      #access_token=${token}
-      &token_type=Bearer
-      &expires_in=3599
-      &scope=https://www.googleapis.com/auth/calendar.readonly%20https://www.googleapis.com/auth/calendar.events%20https://www.googleapis.com/auth/calendar%20https://www.googleapis.com/auth/calendar.events.readonly
-    */
-    try {
-      console.log('hash', window.location.hash)
-      const params = flow(
-        s => s.replace(/^#/, ''),
-        s => new URLSearchParams(s),
-        p => [...p.entries()],
-        Object.fromEntries
-      )(window.location.hash)
-      console.log('params', params)
-      if (params.access_token && params.token_type && params.expires_in && params.scope) {
-        const paramToToken = {
-          accessToken: params.access_token,
-          tokenType: params.token_type,
-          expiresIn: Number(params.expires_in) * 1000,
-          scope: params.scope.split(' '),
-          createdIn: Date.now(),
-        }
-        setToken(paramToToken)
-        window.history.replaceState(null, '', window.location.pathname)
-        localForage.setItem('token', paramToToken)
-      } else {
-        ;(async() => {
-          const localData: typeof token = await localForage.getItem('token')
-          console.log('localData', localData)
-          if (localData?.accessToken) {
-            setToken(localData)
-          }
-          // localForage.removeItem('token')
-        })()
-      }
-    } catch(e) {
-      console.error(e)
-    }
-  }, [])
-  return (
-    <>
-      <div>
-        <button onClick={loginToGoogle}>loginToGoogle</button>
-        <input
-          type="text"
-          style={{ width:'800px' }}
-          placeholder="username/calendarId(gmailAddress)..."
-          value={$testInterviewInput}
-          onInput={e => $setTestInterviewInput((e.target as HTMLInputElement).value)}
-        />
-        <div>
-          <input type="datetime-local" value={timeMin} onChange={({ target: { value } }) => setTimeMin(value)} />
-          <input type="datetime-local" value={timeMax} onChange={({ target: { value } }) => setTimeMax(value)} />
-          <button onClick={loadEventList}>loadEventList</button>
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+`
+export default class Schedule extends Component {
+  render() {
+    return (
+      <Wrapper className="container is-fluid pt-1">
+        <div className="columns" style={{ height: 'inherit' }}>
+          <div className="column is-3">
+            <div className="pt-1" style={{ marginTop: `${'40px'}`}}>
+              <p className="subtitle">OOO's Calendar</p>
+              <div>
+                뭐라뭐라
+              </div>
+            </div>
+            <div>
+              <label><input type="checkbox" className="checkbox" />전체체크</label>
+              <div className="ml-2">
+                <label><input type="checkbox" className="checkbox" />AAA</label>
+                <label><input type="checkbox" className="checkbox" />BBB</label>
+                <label><input type="checkbox" className="checkbox" />CCC</label>
+                <label><input type="checkbox" className="checkbox" />DDD</label>
+                <label><input type="checkbox" className="checkbox" />EEE</label>
+              </div>
+            </div>
+          </div>
+          <div className="column is-9">
+            <div className="is-flex is-justify-content-flex-end mb-1" style={{ height: `calc(${'40px'})` }}>
+              <button className="button is-normal">연동</button>
+              <button className="button is-normal">연동</button>
+              <button className="button is-normal">연동</button>
+            </div>
+            <div style={{ border: '1px solid #ddd', height: `calc(100% - ${'40px'} - ${'40px'})` }}>Calendar</div>
+            <div className="is-flex is-justify-content-flex-end mt-1" style={{ height: `calc(${'40px'})` }}>
+              <button className="button is-primary">제출하기</button>
+            </div>
+          </div>
         </div>
-      </div>
-      <div>
-        <label>access_token: <span>{accessToken ?? 'no token'}</span></label>
-        <label>isTokenValid: <span>{isTokenValid ? 'true' : 'false'}</span></label>
-      </div>
-      <div>
-        <Timetable eventList={eventList}></Timetable>
-      </div>
-    </>
-  )
+      </Wrapper>
+    )
+  }
 }
